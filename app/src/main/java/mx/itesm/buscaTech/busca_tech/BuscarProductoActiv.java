c4a1aa5cadfb74d2359016e6c29ee27c0e29ec66
JonathanCedillo <A01377844@itesm.mx>
2018-03-13 17:59:11 -0600

    Clase de busqueda avanzada creada.

--- app/src/main/java/mx/itesm/buscaTech/busca_tech/BuscarProductoActiv.java
+++ app/src/main/java/mx/itesm/buscaTech/busca_tech/BuscarProductoActiv.java
@@ -1,7 +1,9 @@
 package mx.itesm.buscaTech.busca_tech;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BuscarProductoActiv extends AppCompatActivity {
 
@@ -11,6 +13,13 @@ public class BuscarProductoActiv extends AppCompatActivity {
         this.setTitle(R.string.strBuscaTuProducto);
         setContentView(R.layout.activity_buscar_producto);
     }
+
+    public void mandarABusquedaAvanzada(View v){
+        Intent intBusquedaAvanzada= new Intent(this,BusquedaAvanzadaActiv.class);
+        startActivity(intBusquedaAvanzada);
+    }
+
+
     public void onBackPressed() {
         super.onBackPressed();
         finish();
