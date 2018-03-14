08b0910b972d502855e1a540a1b67bf727a9ece6
JonathanCedillo <A01377844@itesm.mx>
2018-03-13 18:14:35 -0600

    Clase Busqueda Av

---
+++ app/src/main/java/mx/itesm/buscaTech/busca_tech/BusquedaAvanzadaActiv.java
@@ -0,0 +1,14 @@
+package mx.itesm.buscaTech.busca_tech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BusquedaAvanzadaActiv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setTitle(R.string.strBusquedaAvMayusculas);
+        setContentView(R.layout.activity_busqueda_avanzada);
+    }
+}
